b0f9f49d914e14831d4e08660b4d9d1a9fdcea7b
okidijimmy200 <okidijimmie@gmail.com>
2020-10-07 16:41:41 +0300

    building shopping cart 1

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,6 @@ the BASE_DIR variable.
 '''
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+'''This is the key that you are going to use to store the cart in the user session.'''
+CART_SESSION_ID = 'cart'
