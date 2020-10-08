71f360d951d64bba5b29696b6e04cb29fdf45c1e
okidijimmy200 <okidijimmie@gmail.com>
2020-10-08 14:54:10 +0300

    included the views for cart

---
+++ cart/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+
+PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
+
+# You will use this form to add products to the cart.
+class CartAddProductForm(forms.Form):
+# quantity: This allows the user to select a quantity between one and 20. You use a TypedChoiceField field with coerce=int to convert the input into an integer
+    quantity = forms.TypedChoiceField(
+                                choices=PRODUCT_QUANTITY_CHOICES,
+                                coerce=int)
+# override: This allows you to indicate whether the quantity has to be added to any existing quantity in the cart for this product (False), or whether the
+# existing quantity has to be overridden with the given quantity (True). You use a HiddenInput widget for this field, since you don't want to display it to the user.
+    override = forms.BooleanField(required=False,
+                                  initial=False,
+                                  widget=forms.HiddenInput)
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from shop.models import Product
+from .cart import Cart
+from .forms import CartAddProductForm
 
-# Create your views here.
+# view for adding products to the cart or updating quantities for existing products.
+@require_POST #require_POST decorator to allow only POST requests.
+'''The view receives the product ID as a parameter.'''
+def cart_add(request, product_id):
+    cart = Cart(request)
+# retrieve the Product instance with the given ID
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+# validate CartAddProductForm. If the form is valid, you either add or update the product in the cart
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product,
+                 quantity=cd['quantity'],
+                 override_quantity=cd['override'])
+# view redirects to the cart_detail URL
+    return redirect('cart:cart_detail')
+
+# view to remove items from cart
+@require_POST #You use the require_POST decorator to allow only POST requests.
+# The cart_remove view receives the product ID as a parameter.
+def cart_remove(request, product_id):
+    cart = Cart(request)
+# retrieve the Product instance with the given ID and remove the product from the cart
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+# redirect the user to the cart_detail URL
+    return redirect('cart:cart_detail')
+
+# view to display the current cart and its items.
+def cart_detail(request):
+    cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'],
+                                                                   'override': True})
+    return render(request, 'cart/detail.html', {'cart': cart})
