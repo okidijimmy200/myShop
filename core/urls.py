2d3816176b865fe3789e816b77b82256b20c17bf
okidijimmy200 <okidijimmie@gmail.com>
2020-11-03 15:07:18 +0300

    Using the Rosetta translation interface

--- core/urls.py
+++ core/urls.py
@@ -3,19 +3,22 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
+from django.utils.translation import gettext_lazy as _
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+# language prefix to your URL patterns.
+urlpatterns = i18n_patterns(
+    path(_('admin/'), admin.site.urls),
     #this pattern shd be above shop.urls bse its more restrictive
-    path('cart/', include('cart.urls', namespace='cart')),
-    path('orders/', include('orders.urls',namespace='orders')), #shd b before shop
-    path('payment/', include('payment.urls', namespace='payment')), #shd b before shop
-    path('coupons/', include('coupons.urls', namespace='coupons')), #shd b before shop
+    path(_('cart/'), include('cart.urls', namespace='cart')),
+    path(_('orders/'), include('orders.urls',namespace='orders')), #shd b before shop
+    path(_('payment/'), include('payment.urls', namespace='payment')), #shd b before shop
+    path(_('coupons/'), include('coupons.urls', namespace='coupons')), #shd b before shop
     path('rosetta/', include('rosetta.urls')), #shd be b4 shop
 # URLs for the shop application under a custom namespace named shop.
     path('', include('shop.urls', namespace='shop')),
 
-]
+)
 
 # serve uploaded image files.
 if settings.DEBUG:
--- payment/urls.py
+++ payment/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from . import views
+from django.utils.translation import gettext_lazy as _
 
 app_name = 'payment'
 
 urlpatterns = [
     # url for The view that processes the payment
-    path('process/', views.payment_process, name='process'),
+    path(_('process/'), views.payment_process, name='process'),
     # url for  view to redirect the user if the payment is successful
-    path('done/', views.payment_done, name='done'),
+    path(_('done/'), views.payment_done, name='done'),
     # url for view to redirect the user if the payment is not successful
-    path('canceled/', views.payment_canceled, name='canceled'),
+    path(_('canceled/'), views.payment_canceled, name='canceled'),
 ]
